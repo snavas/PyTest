7b60303c8ae77ec42ee7dfd9eba8266d59ad0e57
Samuel Navas <snavas@users.noreply.github.com>
2020-05-07 18:49:40 +0200

    bidirectional client and server

--- vidgearBiClient.py
+++ vidgearBiClient.py
@@ -1,65 +1,118 @@
 # import required libraries
 from vidgear.gears import NetGear
 from vidgear.gears import VideoGear
+from classes.realsense import RealSense
+import base64
 import cv2
+import numpy as np
 
-stream = VideoGear(source='C:\saest.mp4').start()
+from PIL import Image
+import skimage
+import skimage.io
+import base64
+from io import BytesIO
+import json
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+def encode(image) -> str:
+
+    # convert image to bytes
+    with BytesIO() as output_bytes:
+        PIL_image = Image.fromarray(skimage.img_as_ubyte(image))
+        PIL_image.save(output_bytes, 'JPEG') # Note JPG is not a vaild type here
+        bytes_data = output_bytes.getvalue()
+
+    # encode bytes to base64 string
+    base64_str = str(base64.b64encode(bytes_data), 'utf-8')
+    return base64_str
+
+def decode(base64_string):
+    if isinstance(base64_string, bytes):
+        base64_string = base64_string.decode("utf-8")
+
+    imgdata = base64.b64decode(base64_string)
+    img = skimage.io.imread(imgdata, plugin='imageio')
+    return img
+
+# open any valid video stream(for e.g `test.mp4` file)
+#stream = VideoGear(source='C:\saest.mp4').start()
+device = RealSense("752112070399")
 
 # activate bidirectional mode
 options = {'bidirectional_mode': True}
 
-#define Netgear Client with `receive_mode = True` and defined parameter
-client = NetGear(receive_mode = True, pattern = 1, logging = True, **options)
+# define Netgear Client with `receive_mode = True` and defined parameter
+# client = NetGear(receive_mode=True, pattern=1, logging=True, **options) local
+client = NetGear(address = '192.168.0.31', port = '5454', protocol = 'tcp',  pattern = 1, receive_mode = True, logging = True, **options)
 
 # loop over
 while True:
 
-    try:
-        # read frames from stream
-        frame = stream.read()
+    # prepare data to be sent
+    # target_data = "Hi, I am a Client here."
 
-        # check for frame if Nonetype
-        if frame is None:
-            break
+    # read frames from stream
+    # frame = stream.read()
+    frame = device.getcolorstream()
 
-        #prepare data to be sent
+    # check for frame if Nonetype
+    if frame is None:
+        break
+
+    # retval, buffer = cv2.imencode('.jpg', frame)
+    # target_data = base64.b64encode(buffer)
+    # target_data = base64.b64encode(frame)
+    # target_data = frame
+    #base64_bytes = base64.b64encode(frame)
+    #base64_string = base64_bytes.decode('utf-8')
+    #target_data = base64_string
+    # print(frame)
+
+    target_data = encode(frame)
+    #decoded_data = decode(target_data)
+    #cv2.imshow("Server frame, displayed on the Client machine", decoded_data)
+    # print(img_encoded)
+    #target_data = img_encoded.tostring()
+    #target_data = img_encoded
 
 
-        # receive data from server and also send our data
-        data = client.recv(return_data = frame)
+    # receive data from server and also send our data
+    data = client.recv(return_data=target_data)
 
-        # check for data if None
-        if data is None:
-            break
+    # check for data if None
+    if data is None:
+        break
 
-        # extract server_data & frame from data
-        # server_data, frame = data
+    # extract server_data & frame from data
+    server_data, frame = data
 
-        # again check for frame if None
-        #if frame is None:
-        #    break
+    # again check for frame if None
+    if frame is None:
+        break
 
-        # {do something with the extracted frame and data here}
+    # {do something with the extracted frame and data here}
 
-        # lets print extracted server data
-        #if not(server_data is None):
-        #    print(server_data)
+    # lets print extracted server data
+    if not (server_data is None):
+        print(server_data)
 
-        # Show output window
-        cv2.imshow("Output Frame", data)
+    # Show output window
+    cv2.imshow("Server frame, displayed on the Client machine", frame)
 
-        # check for 'q' key if pressed
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"):
-            break
-
-    except KeyboardInterrupt:
+    # check for 'q' key if pressed
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
         break
 
 # close output window
 cv2.destroyAllWindows()
 
-stream.close()
+device.stop()
 
 # safely close client
 client.close()
--- vidgearBiServer.py
+++ vidgearBiServer.py
@@ -1,49 +1,64 @@
 # import required libraries
-from classes.realsense import RealSense
+#from vidgear.gears import VideoGear
 from vidgear.gears import NetGear
+from classes.realsense import RealSense
 import libs.hand as hand
 import libs.draw as draw
-import cv2
+import numpy as np
+import cv2, asyncio, base64
+
+import base64
+import skimage.io
+
+def decode(base64_string):
+    if isinstance(base64_string, bytes):
+        base64_string = base64_string.decode("utf-8")
+
+    imgdata = base64.b64decode(base64_string)
+    img = skimage.io.imread(imgdata, plugin='imageio')
+    return img
 
 # open any valid video stream(for e.g `test.mp4` file)
-device = RealSense("821212062065")
+#stream = VideoGear(source='C:\saest.mp4').start()
+device = RealSense("752112070399")
 
 # activate bidirectional mode
 options = {'bidirectional_mode': True}
 
 # Define Netgear Server with defined parameters
-server = NetGear(pattern=1, logging=True, **options)
+# server = NetGear(pattern=1, logging=True, **options) # LOCAL
+server = NetGear(address = '0.tcp.eu.ngrok.io', port = '12920', protocol = 'tcp',  pattern = 1, logging = True, **options)
 
 # loop over until KeyBoard Interrupted
 while True:
 
     try:
-        # read frames
-        colorframe = device.getcolorstream()
-        depthframe = device.getdepthstream()
+
+        # read frames from stream
+        frame = device.getcolorstream()
 
         # check for frame if Nonetype
-        if colorframe is None:
+        if frame is None:
             break
 
-        # do something with frame here
-        result, hands, points = hand.getHand(colorframe, depthframe, device.getdepthscale())
-        if hands:
-            cv2.drawContours(result, hands, -1, (0, 255, 0), 2)
-            for p in points:
-                cv2.circle(result, tuple(p), 2, (0, 0, 255))
-        drawings = draw.getDraw(colorframe)
+        # {do something with the frame here}
 
         # prepare data to be sent(a simple text in our case)
-        #target_data = 'Hello, I am a Server.'
+        target_data = 'Hello, I am a Server.'
 
         # send frame & data and also receive data from Client
-        #recv_data = server.send(frame, message=target_data)
-        recv_data = server.send(cv2.bitwise_or(result, drawings))
+        recv_data = server.send(frame, message=target_data)
 
         # print data just received from Client
         if not (recv_data is None):
-            cv2.imshow("Output Frame", recv_data)
+            #print(recv_data)
+            result = decode(recv_data)
+            cv2.imshow("Output Frame", result)
+
+        # check for 'q' key if pressed
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
 
     except KeyboardInterrupt:
         break
@@ -51,7 +66,7 @@ while True:
 # safely close video stream
 device.stop()
 
-# close output window
+#
 cv2.destroyAllWindows()
 
 # safely close server
